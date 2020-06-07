acb8f7d7152a9d6c4b70d212140d81459fc45b76
Irshad Ahmed Mohammed Safeer <irshad.safeer@dnata.com>
2020-06-08 00:51:29 +0400

    Added new class to store price of products A,B,C and D

---
+++ PromotionEngineTDDSln/PromotionEngine/ProductPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    class ProductPrice
+    {
+        public const double PriceOfProdA = 50;
+        public const double PriceOfProdB = 30;
+        public const double PriceOfProdC = 20;
+        public const double PriceOfProdD = 15;
+    }
+}
