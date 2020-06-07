8107ae018ae929e20c28cda0b2875f1c3e21f151
Irshad Ahmed Mohammed Safeer <irshad.safeer@dnata.com>
2020-06-08 00:12:22 +0400

    Import Initial 2 Projects, one for unit testing and other one core promotion engine project

---
+++ PromotionEngineTDDSln/PromotionEngineUnitTest/PEUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineUnitTest
+{
+    [TestClass]
+    public class PEUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
