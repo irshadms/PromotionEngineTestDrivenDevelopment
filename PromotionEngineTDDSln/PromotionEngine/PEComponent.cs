7c6e09104eb5a10416f167b4314e65bd33a54cbb
Irshad Ahmed Mohammed Safeer <irshad.safeer@dnata.com>
2020-06-08 00:28:05 +0400

    Get count of each product in the basket.

--- PromotionEngineTDDSln/PromotionEngine/PEComponent.cs
+++ PromotionEngineTDDSln/PromotionEngine/PEComponent.cs
@@ -10,6 +10,12 @@ namespace PromotionEngine
     {
         public double GetTotal(List<string> ListOfProducts)
         {
+            var frequencyOfProductsInBasket = ListOfProducts.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            frequencyOfProductsInBasket.TryGetValue("A", out int CountOfProdA);
+            frequencyOfProductsInBasket.TryGetValue("B", out int CountOfProdB);
+            frequencyOfProductsInBasket.TryGetValue("C", out int CountOfProdC);
+            frequencyOfProductsInBasket.TryGetValue("D", out int CountOfProdD);
 
             return 0;
         }
